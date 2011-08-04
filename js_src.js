6893932383a147d07c61285cc58f510109b4817c
Marcos Garcia de Lima <marckfree@gmail.com>
2011-08-04 08:20:06 -0300

    Changing array to json object.

--- js_src.js
+++ js_src.js
@@ -10,23 +10,17 @@ w.onload = function () {
 	if (menu) {
 		li = [];
 		a = []
-		items = new Array();
-		items[0] = new Array();
-		items[1] = new Array();
-		items[0][0] = 'Home'; items[1][0] = 'home.html';
-		items[0][1] = 'Contato'; items[1][1] = 'contato.html';	
-		items[0][2] = 'Biografia'; items[1][2] = 'biografia.html';
-		items[0][3] = 'Aulas dadas'; items[1][3] = 'aulas_dadas.html';
-		items[0][4] = 'História'; items[1][4] = 'historia.html';
-		items[0][5] = 'Filosofia'; items[1][5] = 'filosofia.html';
-		items[0][6] = 'Google'; items[1][6] = 'http://www.google.com';
-		items[0][7] = 'Debian'; items[1][7] = 'http://debian.org';
-		n_items0 = items[0].length;
-		n_items1 = items[1].length;
-		
-		if (n_items0 == n_items1) {
-			n_items = n_items0 = n_items1;
-			for (i = 0; i <= n_items-1; i++) {
+		var json_object = {'links':[
+	 								{'link':'Home','url':'home.html'},
+									{'link':'Contato','url':'contato.html'},
+									{'link':'Biografia','url':'biografia.html'},
+									{'link':'Equipe','url':'equipe.html'}
+								   ]
+		};
+		var n_links = json_object.links.length;
+
+		if (n_links) {
+			for (i = 0; i <= n_links-1; i++) {
 				//create list ('li' tag)
 				li[i] = d.createElement('li');
 				li[i].setAttribute('id', 'link' + i);		
@@ -34,9 +28,9 @@ w.onload = function () {
 				
 				//create hyperlinks ('a' tag)
 				a[i] = d.createElement('a');
-				a[i].setAttribute('href', items[1][i]);
-				a[i].setAttribute('title', 'Acesse a página ' + items[0][i].toUpperCase());		
-				a[i].innerHTML = items[0][i] 
+				a[i].setAttribute('href', json_object.links[i].url);
+				a[i].setAttribute('title', 'Acesse a página ' + json_object.links[i].link.toUpperCase());		
+				a[i].innerHTML = json_object.links[i].link; 
 				item = d.getElementById('link' + [i]);
 				item.appendChild(a[i]);
 			}	
