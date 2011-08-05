73af284512f2f7dc9cc73b2d777e70ff2e902aec
Marcos Garcia de Lima <marckfree@gmail.com>
2011-08-05 10:00:26 -0300

    Corrigindo criação de IDs automática.

--- data_json.js
+++ data_json.js
@@ -3,12 +3,21 @@
    by Marcker - marckfree@gmail.com
    MIT License - < http://opensource.org/licenses/mit-license.php > */
   
-var json_object = {'links':[
+var json_oaabject = {'links':[
 							{'link':'Home','url':'home.html'},
 							{'link':'Contato','url':'contato.html'},
 							{'link':'Biografia','url':'biografia.html'},
 							{'link':'Equipe','url':'equipe.html'},
 							{'link':'Skate','url':'skate.html'},
-							{'link':'Programar é legal','url':'programming.html'}
+							{'link':'Programar é legal','url':'programming.html'},
+							{'link':'Programação de computadores','url':'http://python.org'},
+							{'link':'Programas','url':'programas.php'},
+							{'link':'Programação Funcional','url':'prog.php'},
+							{'link':'Matérias clássicas','url':'materias.html'},
+							{'link':'Matérias orgânicas','url':'organ.php'},
+							{'link':'Matérias inorgânicas','url':'inorgan.php'},
+							{'link':'Matérias espaciais','url':'space.php'},
+							{'link':'FS','url':'file:///'},
+							{'link':'A lua de arte','url':'htmlbasic.php'}
 						   ]
 };
